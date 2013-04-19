dc418b596a6a56a0b78611bbd9f0204c3455d3d9
Elton Santos <eltonaxl@hotmail.com>
2013-04-19 11:12:47 -0300

    Comitando com arquivos ruby

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Siapfor::Application.config.secret_token = 'a468114f7f35c26b6680c26736651e41509d137215269db20986b655be33072f06f90eea082630ad1893f756ee20b30c62d40e0e1448777b88e6e050d4f9dbbf'
